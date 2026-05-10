8283fe64abf94c0823e695b833f275941bdda0d7
EsenMat <EsenMat@yandex.ru>
2026-05-10 22:55:11 +0400

    Реализована авторизация пользователя

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -9,4 +9,11 @@ class RegisterForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, redirect
 from data import db_session
 from data.guides import Guides
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.get(User,user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route("/")
@@ -38,9 +55,24 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 def main():
     db_session.global_init("db/civ.db")
-    app.run(port=8080)
+    app.run(port=5000)
 
 
 if __name__ == '__main__':
